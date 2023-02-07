f9ac7e82e21380972ffbb52aeab979c515beb7b7
RobJ <robjjohnsenwork@gmail.com>
2023-02-07 09:04:46 +0700

    Basic Layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,24 @@
+import Book from "./components/Book.component";
+
 function App() {
-  return <div>Hi</div>;
+  return (
+    <div className="w-screen h-screen p-10">
+      <div className="bg-green-200 h-full w-full rounded-xl p-5 text-xl flex flex-col gap-2">
+        <section className="w-full bg-white flex text-center">
+          <div className="w-1/4">Book Name</div>
+          <div className="w-1/4">Author</div>
+          <div className="w-1/4">Rating</div>
+          <div className="w-1/4">Button</div>
+        </section>
+        <section className="w-full bg-white flex flex-col">
+          <div className="w-full text-2xl underline text-center">Book List</div>
+          <div>
+            <Book />
+          </div>
+        </section>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/Book.component.js
@@ -0,0 +1,8 @@
+import React from "react";
+//
+//
+function Book(params) {
+  return <div>Hi, im book</div>;
+}
+
+export default Book;
