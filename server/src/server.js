73c6fb9b6d18c36566987dc689907a8b8d051a61
RobJ <robjjohnsenwork@gmail.com>
2023-02-07 12:29:42 +0700

    mongo connection and functions built

--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,22 @@
 const http = require("http");
 require("dotenv").config();
-const app = require("./app");
+//
+//
 const PORT = process.env.PORT;
+const { mongoConnect } = require("./services/mongo");
+const app = require("./app");
 //
 //
 //
 const server = http.createServer(app);
 //
 //
+async function startServer() {
+  await mongoConnect();
+
+  server.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}.... yeah bois!`);
+  });
+}
 //
-server.listen(PORT, () => {
-  console.log(`Server running on port: ${PORT}.... yeah bois!`);
-});
+startServer();
