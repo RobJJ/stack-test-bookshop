be99ddfb9003f95eb84cfbcea819d167dc28eed6
RobJ <robjjohnsenwork@gmail.com>
2023-02-07 09:10:51 +0700

    Input changes to add Book

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,34 @@
+import React from "react";
 import Book from "./components/Book.component";
-
+//
+//
 function App() {
   return (
     <div className="w-screen h-screen p-10">
-      <div className="bg-green-200 h-full w-full rounded-xl p-5 text-xl flex flex-col gap-2">
+      <div className="bg-blue-200 h-full w-full rounded-xl p-5 text-xl flex flex-col gap-2">
         <section className="w-full bg-white flex text-center">
-          <div className="w-1/4">Book Name</div>
-          <div className="w-1/4">Author</div>
-          <div className="w-1/4">Rating</div>
-          <div className="w-1/4">Button</div>
+          <div className="w-1/4">
+            <input
+              className="text-center w-full"
+              type="text"
+              placeholder="Book Name"
+            />
+          </div>
+          <div className="w-1/4">
+            <input
+              className="text-center w-full"
+              type="text"
+              placeholder="Author"
+            />
+          </div>
+          <div className="w-1/4">
+            <input
+              className="text-center w-full"
+              type="text"
+              placeholder="Rating"
+            />
+          </div>
+          <div className="w-1/4 text-green-500">Add Book</div>
         </section>
         <section className="w-full bg-white flex flex-col">
           <div className="w-full text-2xl underline text-center">Book List</div>
@@ -22,3 +42,8 @@ function App() {
 }
 
 export default App;
+
+// TODO
+//
+// For every book in the bookList Database, we render a book and display it
+//
