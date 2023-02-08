5e9ab1b4f8b024d3c5dfd864c8ff7da6027166cf
RobJ <robjjohnsenwork@gmail.com>
2023-02-08 09:30:58 +0700

    controller?

--- server/src/routes/books/books.controller.js
+++ server/src/routes/books/books.controller.js
@@ -2,3 +2,12 @@ const { addNewBook } = require("../../models/books.model");
 //
 //
 //
+async function httpAddNewBook(req, res) {
+  const book = req.body;
+
+  if (!book.name || !book.author || !book.rating) {
+    return res.status(400).json({
+      error: "Missing required book properties!",
+    });
+  }
+}
