6551c797ca55a9bd94ec00cb614a9bda3e9e8c38
RobJ <robjjohnsenwork@gmail.com>
2023-02-09 10:34:38 +0700

    added Morgan logging package

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const morgan = require("morgan");
 //
 //
 const { api } = require("./routes/api");
@@ -11,11 +12,14 @@ const app = express();
 //
 //
 // Middleware to handle requests coming in!
+// Secruity related middleware CORS
 app.use(
   cors({
     origin: "http://localhost:8000",
   })
 );
+//
+app.use(morgan("combined"));
 //
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
