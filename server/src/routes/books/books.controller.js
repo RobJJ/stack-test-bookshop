5aa65f242944a0379f5ac7b9f9250fb43d06cedb
RobJ <robjjohnsenwork@gmail.com>
2023-02-09 10:19:05 +0700

    added nodemon and script

--- server/src/routes/books/books.controller.js
+++ server/src/routes/books/books.controller.js
@@ -4,7 +4,7 @@ const { addNewBook } = require("../../models/books.model");
 //
 async function httpAddNewBook(req, res) {
   const book = req.body;
-  console.log("(books.controller)request... book: ", book);
+  // console.log("(books.controller)request... book: ", book);
   // conditions
   if (!book.name || !book.author || !book.rating) {
     return res.status(400).json({
